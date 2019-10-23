4e1d1cb9c7f280cf151889da4b89f3756446f4f8
Emma Banuelos <emmabanuelos@icloud.com>
2019-10-23 12:53:54 -0500

    commiting at end of study session

--- script.js
+++ script.js
@@ -2,13 +2,24 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
+     
+      let pilotStatusName = document.getElementById("pilotstatus")
+      let copilotStatusName = document.getElementById("copilotStatus")
+      
+      let inputReady = true;
+
+
       if (pilotNameInput.value === "" || pilotNameInput.value === "" || fuelLevel.value === "" || cargoMass.value === "" ) {
          alert("All fields are required!");
-         event.preventDefault();
+         inputReady = false
+         
       }
    });
 });
